1d0851aea3d6fa8a235f1ea3b5dc993f653442f5
Mirjalol Rakhmonov <manhattan701@gmail.com>
2021-01-29 15:20:58 +0100

    Initial commit, WeatherService controller is being implemented

---
+++ src/main/java/com/mirjalolcode/weatherapp/WeatherApp/WeatherAppApplication.java
@@ -0,0 +1,12 @@
+package com.mirjalolcode.weatherapp.WeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mirjalolcode/weatherapp/WeatherApp/view/MainView.java
@@ -0,0 +1,14 @@
+package com.mirjalolcode.weatherapp.WeatherApp.view;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.UI;
+
+@SpringUI(path = "")
+public class MainView extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
