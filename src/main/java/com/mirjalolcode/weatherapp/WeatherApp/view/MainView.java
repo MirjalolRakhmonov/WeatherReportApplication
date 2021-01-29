369fc302054043cb89b99b1b5eee4a0887369a33
Mirjalol Rakhmonov <manhattan701@gmail.com>
2021-01-29 15:52:36 +0100

    Implementing WeatherLogo in View

--- src/main/java/com/mirjalolcode/weatherapp/WeatherApp/view/MainView.java
+++ src/main/java/com/mirjalolcode/weatherapp/WeatherApp/view/MainView.java
@@ -1,14 +1,54 @@
 package com.mirjalolcode.weatherapp.WeatherApp.view;
 
+import com.vaadin.server.ClassResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
 
 @SpringUI(path = "")
 public class MainView extends UI {
 
+    private VerticalLayout mainLayout;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
+        mainLayout();
+        setHeader();
+        setLogo();
+    }
+
+    private void mainLayout() {
+        mainLayout = new VerticalLayout();
+
+        mainLayout.setWidth("100%");
+        mainLayout.setSpacing(true);
+        mainLayout.setMargin(true);
+        mainLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        setContent(mainLayout);
+    }
+
+    private void setHeader(){
+        HorizontalLayout header = new HorizontalLayout();
+        header.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        Label title = new Label("Weather Application by Mirjalol");
+
+        header.addComponent(title);
+
+        mainLayout.addComponent(header);
+    }
+
+    private void setLogo(){
+        HorizontalLayout logo = new HorizontalLayout();
+        logo.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        Image img = new Image(null, new ClassResource("/weatherLogo.jpg"));
+        logo.setWidth("240px");
+        logo.setHeight("240px");
+
+        logo.addComponent(img);
+        mainLayout.addComponent(logo);
     }
 }
